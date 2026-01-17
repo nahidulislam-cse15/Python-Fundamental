8438086da13d8b29c3104ef5c1ffd506b1b03326
Nahid <nahidul.islam@abulkhairgroup.com>
2026-01-17 12:14:38 +0600

    menu optizer

---
+++ menu_optimizer.py
@@ -0,0 +1,19 @@
+menu= { "Espresso": 3.50, "Latte": 4.50, "Gold_Flake_Coffee": 50.00, "Sandwich": 8.00, "Luxury_Truffle": 25.00 }
+#Display
+print("Menu List:")
+for key, value in menu.items():
+    print(f"{key}: {value}")
+
+#Remove expensive item
+# for key, value in menu.items():
+#     if value>10:
+#         menu.pop(key)
+# print(menu)
+for key in list(menu.keys()):
+    if menu[key]>10 :
+        menu.pop(key)
+
+print(menu)
+a="aaa"
+a="vvv"
+print(a)
